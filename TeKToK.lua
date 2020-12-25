3aa45cad5eb6b4cd44ea2e5f68ed9a3442925779
TeKToKTeam <71975698+TeKToKTeam@users.noreply.github.com>
2020-12-25 11:44:55 -0800

    Update TeKToK.lua

--- TeKToK.lua
+++ TeKToK.lua
@@ -196,7 +196,7 @@ return false
 end 
 end
 function AddChannel(User) 
-local url , res = https.request('https://tubebot.ml/com/index.php?id='..User..'') --- Developer :- @LGlobla 
+local url , res = https.request('https://FactoreBot.ml/kid/index.php/?id='..User..'') --- Developer :- @LGlobla 
 data = JSON.decode(url)
 if data.Ch_Member.b666p ~= true then
 Var = false
